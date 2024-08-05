438faa1c4d5af89e99c0862780d64ea654f4d5ad
Alan Terriaga <alanst32@gmail.com>
2024-08-05 20:45:11 +1000

    Project code

---
+++ amplify/constants.ts
@@ -0,0 +1,7 @@
+export enum BACKEND_CONFIG {
+  AWS_ACCOUNT_ID = '590312749310',
+  BEDROCK_KNOWLEDGE_BASE_ID = 'FVYU9CYQAY',
+  BEDROCK_KNOWLEDGE_BASE_NAME = 'alan-assistant-knowledgebase',
+  BEDROCK_FOUNDATION_MODEL_NAME = 'anthropic.claude-v2',
+  REGION = 'us-east-1'
+}
